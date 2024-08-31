128bb04c70a4421ad098c00148150b12dabe3730
Aromal K Prakash <aromalkprakashofficial@gmail.com>
2024-08-31 14:34:36 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,17 @@
 
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     eslint: {
         ignoreDuringBuilds: true,
     },
-    api: {
-        // Enable API routes
-        externalResolver: true,
-      },
-      proxy: {
-        '/api': {
-          target: 'https://arcane-backend.onrender.com',
-          changeOrigin: true,
-          pathRewrite: { '^/api': '' },
-        },
-      },    
     images: {
         domains: ["res.cloudinary.com"],
     },
     sassOptions: {
         cache: false,
     },
+    
     
 };
 
